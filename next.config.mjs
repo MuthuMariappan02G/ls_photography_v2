5eecd7d73b72592e3e4807f3ac01fba4482b61bf
Muthumariappan <muthumariappan.ganapathi.gove.co>
2025-12-16 12:37:50 +0530

    Latest Code

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    workerThreads: false,
+  },
+  productionBrowserSourceMaps: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
